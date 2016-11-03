272279e2d051e2b411a81100f1d88548b7a822b7
UAEDF-tomc <tom.cornelis@ua.ac.be>
2016-11-03 09:36:34 +0100

    updated trigger plots script

--- analysis/triggerPlots.py
+++ analysis/triggerPlots.py
@@ -19,9 +19,22 @@ def makeCumul(hist):
   return cumulHist
 
 
-def run(inputTree):
+def passTriggers(tree, triggerList):
+  for trigger in triggerList:
+    if not hasattr(tree, trigger): continue
+    if getattr(tree, trigger + '_prescale') != 1:
+      print 'Warning: prescaled trigger: ' + trigger + ' (' + hasattr(tree, trigger + '_prescale') + ')'
+    if getattr(tree, trigger): 
+      return True
+  return False
+
+
+def run(inputTree, triggers, dir):
+  try: os.mkdir(dir)
+  except: pass
   hists = {}
-  ptThresholds = [20,25,30,35,40,50]
+#  ptThresholds = ['10to15','15to20','20to30','30to40','40to50']
+  ptThresholds = ['10to15']
   for ptThreshold in ptThresholds:
     hists[ptThreshold] = {}
     for triggerApplied in [True, False]:
@@ -33,6 +46,14 @@ def run(inputTree):
   # loop over events
   for i in range(inputTree.GetEntries()):
     inputTree.GetEntry(i)
+
+    if inputTree._nLeptons != 3: continue
+    for i in range(3):
+      if inputTree._flavors[i] == 0:
+        if inputTree._miniisolation[i] > 0.2: continue
+        if not inputTree._passedMVA90[i]: continue # this is buggy in the tree
+      else:
+        if not inputTree._istight[i]: continue
      
     # flavor of muon is 1, flavor of electron is 0 
     nMuon = sum(inputTree._flavors[i] for i in range(3))
@@ -41,17 +62,18 @@ def run(inputTree):
     elif nMuon == 1: channel = "emumu"
     elif nMuon == 0: channel = "eee"
 
-    if   nMuon == 3: trigger = inputTree.HLT_TripleMu_12_10_5
-    elif nMuon == 2: trigger = inputTree.HLT_DiMu9_Ele9_CaloIdL_TrackIdL
-    elif nMuon == 1: trigger = inputTree.HLT_Mu8_DiEle12_CaloIdL_TrackIdL
-    elif nMuon == 0: trigger = inputTree.HLT_Ele16_Ele12_Ele8_CaloIdL_TrackIdL
-
     pt = [i for i in inputTree._lPt]
     pt.sort(reverse=True)
     for ptThreshold in ptThresholds:
-      if pt[0] < ptThreshold: continue
+      if isinstance(ptThreshold, int): 
+        if pt[0] < ptThreshold: continue
+      else:
+        ptMin = int(ptThreshold.split('to')[0])
+        ptMax = int(ptThreshold.split('to')[1])
+        if pt[0] < ptMin or pt[0] > ptMax: continue
+
       hists[ptThreshold][False][channel].Fill(pt[2], pt[1])
-      if trigger: hists[ptThreshold][True][channel].Fill(pt[2], pt[1])
+      if passTriggers(inputTree, triggers[nMuon]): hists[ptThreshold][True][channel].Fill(pt[2], pt[1])
 
   ROOT.gROOT.LoadMacro("$CMSSW_BASE/src/Majorana/analysis/tdrstyle.C")
   ROOT.setTDRStyle()
@@ -61,8 +83,7 @@ def run(inputTree):
   ROOT.gStyle.SetPadTopMargin(0.15)
   ROOT.gStyle.SetTitleX(0.005)
   ROOT.gStyle.SetTitleY(0.985)
-  ROOT.gStyle.SetOptTitle(1);
-  
+  ROOT.gStyle.SetOptTitle(1)
   ROOT.gROOT.ForceStyle()
  
 
@@ -81,17 +102,49 @@ def run(inputTree):
 	eff.GetZaxis().SetRangeUser(0, 1);
 	eff.GetXaxis().SetTitle("trailing lepton p_{T} [Gev]")
 	eff.GetYaxis().SetTitle("subleading lepton p_{T} [Gev]")
-	eff.SetTitle(channel + " channel, leading lepton p_{T} > " + str(ptThreshold) + " GeV" + (' (cumulative distributions)' if useCumul else ''))
+        if isinstance(ptThreshold, int): ptRange = "p_{T} > " + str(ptThreshold) + " Gev"
+        else:                            ptRange = ptThreshold.split('to')[0] + ' GeV < p_{T} < ' + ptThreshold.split('to')[1] + ' GeV'
+	eff.SetTitle(channel + " channel, leading lepton " + ptRange + (' (cumulative distributions)' if useCumul else ''))
 	eff.Draw("COLZ TEXT")
 	c1.RedrawAxis()
-	c1.Print(eff.GetName() + ('_cumul' if useCumul else '') + '.pdf')
+	c1.Print(os.path.join(dir, eff.GetName() + ('_cumul' if useCumul else '') + '.pdf'))
+	c1.Print(os.path.join(dir, eff.GetName() + ('_cumul' if useCumul else '') + '.png'))
 
   return True
 
 import glob
 chain = ROOT.TChain('trileptonProducer/trileptonTree')
-listOfFiles = glob.glob('/pnfs/iihe/cms/store/user/tomc/majorana/WZJToLLLNu_TuneCUETP8M1_13TeV-amcnlo-pythia8/crab_20161027_134409/161027_114444/0000/trilepton*.root')
+listOfFiles = glob.glob('/pnfs/iihe/cms/store/user/tomc/majorana/WZJToLLLNu_TuneCUETP8M1_13TeV-amcnlo-pythia8/crab_test2/161030_121836/0000/trilepton*.root')
 for i in listOfFiles:
   chain.Add(i)
 
-run(chain)
+# 3l triggers only
+triggers_3l = {3: ['HLT_TripleMu_12_10_5'],
+               2: ['HLT_DiMu9_Ele9_CaloIdL_TrackIdL'],
+               1: ['HLT_Mu8_DiEle12_CaloIdL_TrackIdL'],
+               0: ['HLT_Ele16_Ele12_Ele8_CaloIdL_TrackIdL']}
+run(chain, triggers_3l, '3l')
+
+
+# 2l triggers
+triggers_2l = {2: ['HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ', 'HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ','HLT_Mu30_TkMu11','HLT_Mu40_TkMu11'],
+               1: ['HLT_Mu8_TrkIsoVVL_Ele17_CaloIdL_TrackIdL_IsoVL'],
+               0: ['HLT_DoubleEle37_Ele27_CaloIdL_GsfTrkIdVL','HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ','HLT_DoubleEle24_22_eta2p1_WPLoose_Gsf','HLT_DoubleEle33_CaloIdL_GsfTrkIdVL']}
+triggers_3l2l = {3: triggers_3l[3] + triggers_2l[2],
+                 2: triggers_3l[2] + triggers_2l[2] + triggers_2l[1],
+                 1: triggers_3l[1] + triggers_2l[1] + triggers_2l[0],
+                 0: triggers_3l[0] + triggers_2l[0]}
+#run(chain, triggers_3l2l, '3l2l')
+
+
+# 1l triggers (removed HLT_IsoMu24_eta2p1, HLT_IsoTkMu24_eta2p1, HLT_Ele30_WPTight_Gsf, HLT_Ele30_eta2p1_WPTight_Gsf)
+triggers_1l = {1: ['HLT_Mu50','HLT_TkMu50','HLT_IsoMu27','HLT_IsoTkMu27','HLT_IsoMu24','HLT_IsoMu24','HLT_IsoMu22_eta2p1','HLT_IsoTkMu22_eta2p1','HLT_IsoTkMu22','HLT_Mu45_eta2p1'],
+               0: ['HLT_Ele32_WPTight_Gsf','HLT_Ele32_eta2p1_WPTight_Gsf','HLT_Ele27_WPTight_Gsf','HLT_Ele27_eta2p1_WPTight_Gsf','HLT_Ele27_eta2p1_WPTight_Gsf','HLT_Ele25_eta2p1_WPTight_Gsf','HLT_Ele25_WPTight_Gsf']}
+triggers_3l2l1l = {3: triggers_3l2l[3] + triggers_1l[1],
+                   2: triggers_3l2l[2] + triggers_1l[1] + triggers_1l[0],
+                   1: triggers_3l2l[1] + triggers_1l[1] + triggers_1l[0],
+                   0: triggers_3l2l[0] + triggers_1l[0]}
+#run(chain, triggers_3l2l1l, '3l2l1l')
+
+
+
