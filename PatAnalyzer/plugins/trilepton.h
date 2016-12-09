6dada8a2b81cd944a5d703c0e323db3c19b46074
martinavit <martina.vit@cern.ch>
2016-12-09 12:17:50 +0100

    Update trilepton.h

--- PatAnalyzer/plugins/trilepton.h
+++ PatAnalyzer/plugins/trilepton.h
@@ -141,6 +141,7 @@ private:
     
     edm::EDGetTokenT<reco::GenParticleCollection>    genparticleToken;
     edm::EDGetTokenT<edm::ValueMap<float>>           electronMvaIdMapToken;
+    edm::EDGetTokenT<edm::ValueMap<float>>           mvaValuesMapToken_HZZ;
     edm::EDGetTokenT<edm::ValueMap<bool>>  	     electronMvaIdMap80Token;
     edm::EDGetTokenT<edm::ValueMap<bool>>  	     electronMvaIdMap90Token;
     edm::EDGetTokenT<edm::ValueMap<bool>>            electronCutBasedIdMapTightToken;
