5167137289ed4f64a0328a6db714f8cdc368e058
UAEDF-tomc <tom.cornelis@ua.ac.be>
2017-02-15 17:10:40 +0100

    script to combine different 1D eff maps

--- analysis/triggerEfficiency/combinePlots.py
+++ analysis/triggerEfficiency/combinePlots.py
@@ -8,19 +8,38 @@ style.setDefault()
 file = ROOT.TFile(os.path.join("triggerEfficiencies.root"))
 file.cd()
  
-samples = ['WZ','WJets','MET','JetHT']
+samples = ['WZ','WJets','MET','JetHT','JetHT-met30']
 for l in ['e','mu']:
-  histos = []
-  i = 0
+  graphs = ROOT.TMultiGraph()
+  graphs.forLegend = []
+  color = 0
   for sample in samples:
-    keys = [k for k in file.GetListOfKeys() if (k.GetName() == sample + '/1l_' + l)]
+    keys = [k for k in file.GetListOfKeys() if (k.GetName().count(sample + '/1l_' + l)) and not k.GetName().count('etaBinned')]
     if len(keys) == 1:
       print 'Adding ' + sample
-      i += 1
-      histos.append(keys[0].ReadObj())
-      histos[-1].texName = sample
-      histos[-1].style   = style.errorStyle(i)
-      histos[-1].xTitle  = 'p_T(l)'
-      histos[-1].yTitle  = 'eff'
-  histos[0].SetName('plots/combine/eff_1l_' + l)
-  plot.draw1D(histos, legend=(0.6, 0.15, 0.85, 0.6))
+      color += 1
+      graph = keys[0].ReadObj().CreateGraph()
+      graph.SetMarkerStyle(20)
+      graph.SetMarkerColor(color)
+      graph.SetLineColor(color)
+      graphs.forLegend.append((graph, sample))
+      graphs.Add(graph, 'p')
+
+  c = ROOT.TCanvas(l, l)
+  graphs.SetTitle(';p_{T}(l) [GeV];1l efficiency')
+  graphs.Draw('a')
+
+  legendCoordinates = (0.40,0.15,0.92,0.4)
+  legend = ROOT.TLegend(*legendCoordinates)
+  legend.SetFillStyle(0)
+  legend.SetShadowColor(ROOT.kWhite)
+  legend.SetBorderSize(0)
+  for (g, n) in graphs.forLegend:
+    legend.AddEntry(g, n)
+    legend.Draw()
+
+  dir = 'plots/combine/'
+  try:    os.makedirs(dir)
+  except: pass
+  c.Print(dir + l + '.pdf')
+  c.Print(dir + l + '.png')
